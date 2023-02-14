4e295bedb39a1d551b75f240945c56de9ecbc253
Luke O'Leary <luke.oleary1@gmail.com>
2023-02-14 16:23:21 +0000

    Init Commit

---
+++ app/src/main/java/ie/ul/firebasedemo2/MainActivity.java
@@ -0,0 +1,55 @@
+package ie.ul.firebasedemo2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClickSignIn(View view) {
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build()
+        );
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN
+        );
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                System.out.println("Sign in Successful! \nName:" + user.getDisplayName() + "\n Email:" + user.getEmail() + "\n id:" + user.getUid());
+            } else {
+                if (response == null) {
+                    System.out.println("Sign in cancelled by user");
+                } else {
+                    System.out.println("Sign in failed");
+                }
+                return;
+            }
+        }
+    }
+}
